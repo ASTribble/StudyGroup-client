f16672465d0e4dacbaaf5a0150f02f2547f674fd
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-02-15 13:46:33 -0800

    added route history to deleteSession function

---
+++ src/actions/delete-sessions.js
@@ -0,0 +1,27 @@
+import {API_BASE_URL} from '../config'; 
+import {sessionRequest, sessionError, getSessions} from './get-sessions';
+
+
+export const deleteSessionAPI = id => {
+   
+    const query = {
+        method: 'DELETE', 
+        body: JSON.stringify({id}),
+        headers: new Headers({'Content-Type': 'application/json'})
+    }; 
+
+    return fetch(`${API_BASE_URL}/sessions/${id}`, query);
+};  
+
+export const deleteSessionById= id => dispatch => {
+    dispatch(sessionRequest);
+    return deleteSessionAPI(id)
+    .then(res => {
+        if(!res.ok){
+            return new Promise.reject(res)
+        }
+        return res.json();
+    })
+    .then(()=> dispatch(getSessions()))
+    .catch(err => dispatch(sessionError(err)));
+}
--- src/components/session-detail/session-detail.js
+++ src/components/session-detail/session-detail.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import LocationTag from './location-tag';
 import SessionNotes from './session-notes';
 import NameList from './name-list';
 import HomeNav from '../home-nav/home-nav';
+
 import {getSessionById} from '../../actions/get-sessions';
+import {deleteSessionById} from '../../actions/delete-sessions'
 
 
 export class SessionDetail extends React.Component{
@@ -12,6 +15,12 @@ export class SessionDetail extends React.Component{
     componentDidMount(){
         this.props.dispatch(getSessionById(this.props.id));
       }
+
+    deleteSession(e){
+        e.preventDefault();
+        this.props.dispatch(deleteSessionById(this.props.id))
+            .then(() => this.props.history.push('/'));
+    }  
    
    render(){
         return (
@@ -24,6 +33,11 @@ export class SessionDetail extends React.Component{
                 < LocationTag />
                 < SessionNotes />
                 < NameList />
+                <div id='delete-button-div'>
+                    <Link to='/'>
+                    <button className='delete-button' onClick={(e)=>this.deleteSession(e)}> Delete Session </button>
+                    </Link>
+                </div>
             </div>    
         )
     }    
