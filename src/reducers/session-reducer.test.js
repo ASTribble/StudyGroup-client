b9f70b55a9ddd9fce7e9d3450fb5613f67e39117
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-21 13:20:37 -0700

    added tests for sessionRequest and sessionSuccess

---
+++ src/reducers/session-reducer.test.js
@@ -0,0 +1,39 @@
+import reducer from './session';
+import { 
+    sessionRequest,
+    sessionSuccess,
+    singleSessionSuccess,
+    sessionError
+  } from '../actions/get-sessions';
+
+
+describe('It should update state', () => {
+
+    it('Should set loading to true on sessionRequest', () => {
+        const state = {
+            loading: false,
+        };
+        const action = sessionRequest();
+        const newState = reducer(state, action);
+
+        expect(newState.loading).toEqual(true);
+    });
+
+    it('Should set session on sessionSuccess', () => {
+        const state = {};
+        const sessions = ['1', '2', '3'];
+        const action = sessionSuccess(sessions);
+
+        const newState = reducer(state, action);
+        expect(newState.sessions).toEqual(sessions);
+    });
+
+    it('Should set loading to false on sessionSuccess', () => {
+        const state = {loading: true};
+        const sessions = ['1', '2', '3'];
+        const action = sessionSuccess(sessions);
+
+        const newState = reducer(state, action);
+        expect(newState.loading).toEqual(false);
+    });
+})
